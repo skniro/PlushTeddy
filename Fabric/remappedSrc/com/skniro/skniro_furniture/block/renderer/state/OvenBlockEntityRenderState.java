5dbc388fca79c68dc2cac3b5deb42503d9d2f6a4
skniro <46392259+skniro@users.noreply.github.com>
2026-02-22 21:18:06 +0800

    First release.

--- Fabric/remappedSrc/com/skniro/skniro_furniture/block/renderer/state/OvenBlockEntityRenderState.java
+++
@@ -1,9 +0,0 @@
-package com.skniro.skniro_furniture.block.renderer.state;
-
-
-import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
-import net.minecraft.client.renderer.item.ItemStackRenderState;
-
-public class OvenBlockEntityRenderState extends BlockEntityRenderState {
-    public final ItemStackRenderState item = new ItemStackRenderState();
-}
